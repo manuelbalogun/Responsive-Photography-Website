a1ca6218ad3efe2268f5dd23dc51d1ef3c0d08fa
Ayodeji Balogun <manuelbalogun@gmail.com>
2022-07-29 01:26:26 +0000

    Nav Bar and Hero Section Complete

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+alert("Deji is freaking awesome!");
 
+// ========== Change Nav style on scroll ==========
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0)
+});
