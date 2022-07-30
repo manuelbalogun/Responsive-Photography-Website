f341c0eedb16adc6d795c40359b5399acad62049
Ayodeji Balogun <manuelbalogun@gmail.com>
2022-07-30 19:04:50 +0000

    Small frames and Circular buttons added

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ alert("Deji is freaking awesome!");
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0)
 });
+
+
+// ========== Circular Buttons ==========
+
+const textButtons = document.querySelectorAll('.contact_btn');
+
+textButtons.forEach(textButton => {
+    let text = textButton.querySelector('p');
+
+    text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join('')
+});
+
